113742ec1e40267e88eee9f14e74a827e450d608
narah28 <112137060+narah28@users.noreply.github.com>
2022-09-28 16:02:13 -0400

    HW4_first draft

--- solution-hw4/javascript/hw4.js
+++ solution-hw4/javascript/hw4.js
@@ -1,26 +1,50 @@
+/* ------------------- THIS SECTION IS ENTIRELY NEW FOR HW4 --------------------- */
+
+/* defining an array named cart */
+let cart = [
+]
+
+/* getting the rolltype */
 const queryString = window.location.search;
-    console.log(queryString);
 const params = new URLSearchParams(queryString);
-    console.log(params);
 const rolltype = params.get('roll');
-    console.log(rolltype);
+    console.log("This is the rolltype: " + rolltype);
 
 
+/* heading of each detail page */
+const headerElement = document.querySelector('#productPageTitle');
+if (headerElement === null) {
+} else {
+headerElement.innerText = rolltype + " Cinnamon Roll"
+};
+
+/* prodcut image of each detail page */
+const productImage = document.querySelector('.product_photo img');
+if (productImage === null) {
+} else {
+productImage.src = './assets/' + rolls[rolltype].imageFile;
+productImage.alt = rolltype + " cinnamon roll image"
+};
 
 
+/* product base price of each detail page */
+const productPrice = document.querySelector('#cartTotalPrice');
+if (productPrice === null) {
+} else{
+productPrice.innerText = "$" + rolls[rolltype].basePrice
+};
 
 
-
-
+/* ------------------- INSERTING THE OPTIONS FOR BOTH DROPDOWNS --------------------- */
 
 
 // for storing glazing option names/values 
 let allGlazing = [
-]
+];
 
 // for storing pack size option names/values
 let allPack = [
-]
+];
 
 // finding the glazing select element in html
 let selectElementGlazing = document.querySelector('#glazing')
@@ -57,19 +81,17 @@ let addPackOptions = [
 
 //pushing the addGlazingOptions array to the allGlazing array one by one
 allGlazing.push(addGlazingOptions[0], addGlazingOptions[1], addGlazingOptions[2], addGlazingOptions[3]);
-//console.log(allGlazing)
 
 //pushing the addGlazingOptions array to the allGlazing array one by one
 allPack.push(addPackOptions[0], addPackOptions[1], addPackOptions[2], addPackOptions[3]);
-//console.log(allPack)
-
 
 //adding the options with a for loop_glazing
+if (selectElementGlazing === null) {
+} else {
 for (let i = 0; i < allGlazing.length; i++) {
     let optionGlazing = document.createElement('option');
     optionGlazing.text = allGlazing[i].glazingOption;
     optionGlazing.value = allGlazing[i].price;
-    console.log(optionGlazing)
     selectElementGlazing.add(optionGlazing);
 
 }
@@ -81,36 +103,47 @@ for (let i = 0; i < allPack.length; i++) {
 
     selectElementSize.add(optionPack);
 }
-
-// calling the base price, getting rid of the dollar sign, and changing the type to number
-let basePrice = document.querySelector('#cartTotalPrice').innerText
-//console.log(basePrice)
-basePrice = Number(basePrice.substring(1)) //just getting rid of the dollar sign and changing it to a number
-//console.log(basePrice)
-//console.log(typeof basePrice)
+};
+/* ------------------- CALCULATING THE TOTAL PRICE OF THE PRODUCT --------------------- */
+/* HW4: fixed this part just to reflect the base price */
 
 //for when the user clicks glazing option
 function glazingChange(element) {
     const priceChange = Number(element.value);
     const packChange = Number(document.querySelector('#size').value);
-    let updatedPrice = Number((priceChange + basePrice) * packChange).toFixed(2);
-    //console.log(updatedPrice)
-    //console.log(typeof updatedPrice)
-    let totalPrice = document.querySelector('#cartTotalPrice')
-    totalPrice.innerText = "$" + updatedPrice
+    let updatedPrice = Number((priceChange + rolls[rolltype].basePrice) * packChange).toFixed(2);
+    let totalPrice = document.querySelector('#cartTotalPrice');
+    totalPrice.innerText = "$" + updatedPrice;
 }
+
 
 //for when the user clicks pack size option
 function sizeChange(element) {
     const packChange = Number(element.value);
     const priceChange = Number(document.querySelector('#glazing').value);
-    let updatedPrice = Number((priceChange + basePrice) * packChange).toFixed(2);
-    //console.log(updatedPrice)
-    let totalPrice = document.querySelector('#cartTotalPrice')
-    totalPrice.innerText = "$" + updatedPrice
+    let updatedPrice = Number((priceChange + rolls[rolltype].basePrice) * packChange).toFixed(2);
+    let totalPrice = document.querySelector('#cartTotalPrice');
+    totalPrice.innerText = "$" + updatedPrice;
 }
 
+/* ------------------- ADD TO CART (CONSOLE PART) --------------------- */
+/* HW4: this is a newly added part*/
+/* defining the class Roll */
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
 
-
-
-
+/* adding items to the cart array : got the selectElement code from https://stackoverflow.com/questions/5913/getting-the-text-from-a-drop-down-box */
+function addItemsToCart() {
+    let currentGlazing = selectElementGlazing.options[selectElementGlazing.selectedIndex].text;
+    let currentSize = selectElementSize.options[selectElementSize.selectedIndex].text;
+    let addToCart = new Roll(rolltype, currentGlazing, currentSize, rolls[rolltype].basePrice);
+    
+    cart.push(addToCart);
+    console.log(cart)
+}
