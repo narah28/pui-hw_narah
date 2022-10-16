a1ee75e3cac8cd2b379ede3c187b3c31ad23b896
narah28 <112137060+narah28@users.noreply.github.com>
2022-10-15 21:53:25 -0400

    bug fix
    added let cart=[] at the top of docs

--- solution-hw6/javascript/cart.js
+++ solution-hw6/javascript/cart.js
@@ -1,3 +1,4 @@
+let cart = [];
 
 function retrieveFromLocalStorage() {
     const stringCart  = localStorage.getItem('storedCart');
--- solution-hw6/javascript/hw6.js
+++ solution-hw6/javascript/hw6.js
@@ -1,5 +1,6 @@
 /* ------------------- THIS SECTION IS ENTIRELY NEW FOR HW4 --------------------- */
 
+let cart=[];
 
 if (localStorage.getItem('storedCart') != null) {
     retrieveFromLocalStorage();
