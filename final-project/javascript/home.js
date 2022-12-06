88b72247add1daac4b72098ef40e3a15ec54a536
narah28 <112137060+narah28@users.noreply.github.com>
2022-12-05 21:10:28 -0500

    tunnelvision

--- final-project/javascript/home.js
+++ final-project/javascript/home.js
@@ -1,5 +1,76 @@
 function movePage() {
-    window.location.href = "./presbyopia.html";
+    window.location.href = "./tunnelvision.html";
   }
 
-  
+
+//   $(document).ready(function() {
+//     $(this).mousemove(function(e) {
+//       $("#textPart").css({
+//         "top": e.pageY - 250,
+//         "left": e.pageX - 250
+//       })
+//     }).mousedown(function(e) {
+//       switch (e.which) {
+//           case 1:
+//               $("#textPart").toggleClass("light-off");
+//               break;
+//           case 2:
+//               console.log('Middle Mouse button pressed.');
+//               break;
+//           case 3:
+//               console.log('Right Mouse button pressed.');
+//               break;
+//           default:
+//               console.log('You have a strange Mouse!');
+//       }
+//     })
+//   })
+
+
+  //this is a sample flashlight effect
+
+// $(document).ready(function() {
+//     $(this).mousemove(function(e) {
+//       $("#instText").css({
+//         "top": e.pageY - 250,
+//         "left": e.pageX - 250
+//       })
+//     }).mousedown(function(e) {
+//       switch (e.which) {
+//           case 1:
+//               $("#instText").toggleClass("light-off");
+//               break;
+//           case 2:
+//               console.log('Middle Mouse button pressed.');
+//               break;
+//           case 3:
+//               console.log('Right Mouse button pressed.');
+//               break;
+//           default:
+//               console.log('You have a strange Mouse!');
+//       }
+//     })
+//   })
+
+$(document).ready(function() {
+    $(this).mousemove(function(e) {
+      $("#light").css({
+        "top": e.pageY - 50,
+        "left": e.pageX - 50
+      })
+    }).mousedown(function(e) {
+      switch (e.which) {
+          case 1:
+              $("#light").toggleClass("light-off");
+              break;
+          case 2:
+              console.log('Middle Mouse button pressed.');
+              break;
+          case 3:
+              console.log('Right Mouse button pressed.');
+              break;
+          default:
+              console.log('You have a strange Mouse!');
+      }
+    })
+  })
