f5a1f01693861189496cdc96d02b7e580a58dd47
narah28 <112137060+narah28@users.noreply.github.com>
2022-12-08 14:58:39 -0500

    responsive final touch

--- final-project/javascript/home.js
+++ final-project/javascript/home.js
@@ -1,3 +1,4 @@
+// for navigating pages
 function movePage() {
     window.location.href = "./tunnelvision.html";
   }
@@ -30,54 +31,3 @@ $(document).ready(function() {
     $('.woohoo').textillate();
   })
   $('.woohoo').textillate({ in: {effect:'rollIn'}});
-
-
-
-//   $(document).ready(function() {
-//     $(this).mousemove(function(e) {
-//       $("#textPart").css({
-//         "top": e.pageY - 250,
-//         "left": e.pageX - 250
-//       })
-//     }).mousedown(function(e) {
-//       switch (e.which) {
-//           case 1:
-//               $("#textPart").toggleClass("light-off");
-//               break;
-//           case 2:
-//               console.log('Middle Mouse button pressed.');
-//               break;
-//           case 3:
-//               console.log('Right Mouse button pressed.');
-//               break;
-//           default:
-//               console.log('You have a strange Mouse!');
-//       }
-//     })
-//   })
-
-
-  //this is a sample flashlight effect
-
-// $(document).ready(function() {
-//     $(this).mousemove(function(e) {
-//       $("#instText").css({
-//         "top": e.pageY - 250,
-//         "left": e.pageX - 250
-//       })
-//     }).mousedown(function(e) {
-//       switch (e.which) {
-//           case 1:
-//               $("#instText").toggleClass("light-off");
-//               break;
-//           case 2:
-//               console.log('Middle Mouse button pressed.');
-//               break;
-//           case 3:
-//               console.log('Right Mouse button pressed.');
-//               break;
-//           default:
-//               console.log('You have a strange Mouse!');
-//       }
-//     })
-//   })
